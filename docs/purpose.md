c2ccc04bfe94af0aef901468a1284eeedc84f1aa
Stefan Walther <Stefan.Walther@qlik.com>
2017-01-04 23:57:18 +0100

    docs: Update general docs on README.md

---
+++ docs/purpose.md
@@ -0,0 +1,5 @@
+<!-- Purpose -->
+
+_sammler-scheduler_ publishes messages to RabbitMQ based on scheduled events (jobs).
+
+It is also the responsibility of _sammler-scheduler_ to store the initial state of jobs (using _sammler-jobs-service_).
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const schedule = require( 'node-schedule' );
+const schedule = require('node-schedule');
 const amqp = require('amqplib');
 const URL = process.env.SAMMLER_RABBITMQ_URL || 'amqp://guest:guest@localhost:5672';
 
@@ -10,18 +10,21 @@ function encode(doc) {
 }
 
 /**
- * Post a very basic message `sammler-middleware-github` to s5r-rabbitmq.
+ * Post a very basic message `sammler-strategy-github` to s5r-rabbitmq.
  */
-let j = schedule.scheduleJob( '* * * * *', function() {
-  let open = amqp.connect( URL );
+let j = schedule.scheduleJob('* * * * *', function () {
+  let open = amqp.connect(URL);
   let queue = 'queue';
-  open.then( conn => {
+  open.then(conn => {
     return conn.createChannel()
-      .then( (channel) => {
+      .then((channel) => {
         return Promise.all([
           channel.assertQueue(queue),
-          channel.sendToQueue(queue, encode('sammler-middleware-github'), {persistent: true})
+          channel.sendToQueue(queue, encode('sammler-strategy-github'), {persistent: true})
         ]);
       });
-  });
-} );
+  })
+    .catch( (err) => {
+      console.log('error connecting to RabbitMG', err);
+    });
+});
