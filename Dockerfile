286dd24f7ec27a0565014e54c3e3637b70f091d8
Stefan Walther <Stefan.Walther@qlik.com>
2017-01-05 17:50:21 +0100

    feat: add nodemon watching & re-building in dev env

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
 FROM kkarczmarczyk/node-yarn:7.2-slim
 MAINTAINER Stefan Walther <swr.nixda@gmail.com>
 
+RUN yarn global add nodemon
+
 ENV HOME /home
 RUN mkdir -p $HOME
-
 WORKDIR $HOME
 
 COPY package.json yarn.lock ./
 
-RUN yarn global add nodemon && \
-    yarn
+RUN yarn
 
 COPY /src ./src
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const schedule = require('node-schedule');
 const amqp = require('amqplib');
-const URL = process.env.SAMMLER_RABBITMQ_URI || 'amqp://guest:guest@localhost:5672';
+
+const RABBITMQ_URI = process.env.SAMMLER_RABBITMQ_URI || 'amqp://guest:guest@localhost:5672';
+// const JOBS_SERVICE_URI = process.evn.SAMMLER_JOBS_SERVICE_URI ;
 
 const rule = new schedule.RecurrenceRule();
 rule.minute = 1;
@@ -13,18 +15,69 @@ function encode(doc) {
  * Post a very basic message `sammler-strategy-github` to s5r-rabbitmq.
  */
 schedule.scheduleJob('* * * * *', () => {
-  const open = amqp.connect(URL);
-  const queue = 'queue';
-  open.then(conn => {
-    return conn.createChannel()
-      .then(channel => {
-        return Promise.all([
-          channel.assertQueue(queue),
-          channel.sendToQueue(queue, encode('sammler-strategy-github'), {persistent: true})
-        ]);
-      });
-  })
+
+  const cfg = {
+    server: RABBITMQ_URI,
+    exchange: {
+      type: 'direct',
+      name: 'topics_logs'
+    },
+    key: 'kern.critical',
+    message: {
+      foo: 'bar',
+      bar: 'baz'
+    }
+  };
+  // Todo: Create the job first, so that we also have an Id
+  publishMessage(cfg)
+    .then(() => {
+      console.log('publish message done');
+    })
     .catch(err => {
-      console.log('error connecting to RabbitMG', err);
+      console.warn('err returned', err);
     });
+
+  // const open = amqp.connect(RABBITMQ_URI);
+  // const queue = 'queue';
+  // open.then(conn => {
+  //   return conn.createChannel()
+  //     .then(channel => {
+  //       return Promise.all([
+  //         channel.assertQueue(queue),
+  //         channel.sendToQueue(queue, encode('sammler-strategy-github'), {persistent: true})
+  //       ]);
+  //     });
+  // })
+  //   .catch(err => {
+  //     console.log('error connecting to RabbitMG', err);
+  //   });
 });
+
+// Todo: Validate opts
+// Todo: Set some default values for opts
+/**
+ * Post a message to RabbitMq.
+ * @param {object} opts - Configuration to use to publish a message.
+ * @param {object} opts.server - RabbitMQ server. If a string is passed, it's just the URI.
+ * @param {object} opts.exchange - Information about the exchange.
+ * @param {string} opts.exchange.type - 'topic', 'direct'
+ * @param {string} opts.exchange.name - Name of the exchange.
+ * @param {string} opts.key - Key to publish the message.
+ * @param {object} opts.message - The message to post.
+ */
+function publishMessage(opts) {
+  return amqp.connect(opts.server)
+    .then(conn => {
+      conn.createChannel()
+        .then(ch => {
+          ch.assertExchange(opts.exchange.name, opts.exchange.type, {durable: true});
+          ch.publish(opts.exchange.name, opts.key, encode(opts.message));
+          console.log(" [x] Sent %s:'%s'", opts.key, JSON.stringify(opts.message, null)); // eslint-disable-line quotes
+          setTimeout(() => {
+            conn.close();
+            console.log('connection closed');
+          }, 500);
+        });
+    });
+}
+
